88ffbcefa95911e1a972689f62e8c234d8f06ee1
ControlledChaos <greg@ccdzine.com>
2025-05-12 09:01:38 -0700

    Admin controller.

--- plugin.php
+++ plugin.php
@@ -129,6 +129,22 @@ class Posts_Lists extends Plugin {
 	}
 
 	/**
+	 * Admin controller
+	 *
+	 * Change the text of the `<title>` tag.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @global object $L The Language class.
+	 * @global array $layout
+	 * @return string Returns the head content.
+	 */
+	public function adminController() {
+		global $L, $layout, $site;
+		$layout['title'] = $L->get( 'Posts Lists Guide' ) . ' | ' . $site->title();
+	}
+
+	/**
 	 * Admin info pages
 	 *
 	 * @since  1.0.0
